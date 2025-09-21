cf8d3ef31d761aa634541f05ad19a7751043c7c6
Martín V <mvpub@protonmail.com>
2025-09-21 14:43:55 -0300

    Placeholder para juego Carrera

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -56,21 +56,46 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
       ),
       body: SingleChildScrollView(
         child: Column(
+          spacing: 16,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
               onTap: udpInitialized ? () => context.push('/perspectiva') : null,
               child: Card(
                 color: Colors.deepPurple,
-                child: ListTile(
-                  title: Text('Perspectiva',
-                      style: Theme.of(context).textTheme.displaySmall),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/img/perspectiva.png'),
-                      Text('Usa el giroscopio para simular perspectiva')
-                    ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text('Perspectiva',
+                        style: Theme.of(context).textTheme.displaySmall),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset('assets/img/perspectiva.png'),
+                        Text('Usa el giroscopio para simular perspectiva', style: Theme.of(context).textTheme.titleLarge,)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: udpInitialized ? () => context.push('/carrera') : null,
+              child: Card(
+                color: Colors.lightGreen,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text('Carrera',
+                        style: Theme.of(context).textTheme.displaySmall),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Image.asset('assets/img/carrera.png'),
+                        SizedBox(height:100, child: Placeholder()),
+                        Text('Juego de carrera multipantalla (1x3)', style: Theme.of(context).textTheme.titleLarge,)
+                      ],
+                    ),
                   ),
                 ),
               ),
